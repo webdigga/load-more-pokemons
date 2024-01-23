19e7cbc2d59e22ccfdd973ed6ad99d00b5027f73
David White <webdigga42@gmail.com>
2024-01-23 16:43:15 +0000

    Adding basic first set of data

--- src/pokemon-list.jsx
+++ src/pokemon-list.jsx
@@ -2,6 +2,36 @@
 // Make sure to replace limit and offset with the appropriate values
 // https://pokeapi.co/api/v2/pokemon?limit=5&offset=0
 
-const PokemonList = () => {};
+
+// TODO - move fetch out to separate file
+// TODO - add the load more button
+// TODO - styling
+
+import { useEffect, useState } from "react";
+
+const fetchItems = async() => {
+	const items = await fetch('https://pokeapi.co/api/v2/pokemon?limit=5&offset=0');
+	const pokemons = await items.json();
+
+	return pokemons;
+}
+
+const PokemonList = () => {
+	const [pokemons, setPokemons] = useState([]);
+
+	useEffect(() => {
+		fetchItems().then((response) => {
+			setPokemons(response.results);
+		});
+	});
+
+	return (
+		<ul>
+			{pokemons.map((pokemon) => (
+				<li key={pokemon.url}>{pokemon.name}</li>
+			))}
+		</ul>
+	)
+};
 
 export default PokemonList;
